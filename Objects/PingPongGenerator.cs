a2bde4c82602eb6edbdbfb1094e1023015402d96
RubyBe <sidbenavente@hotmail.com>
2016-07-18 12:59:33 -0700

    add specs for the PingPongGenerater class

---
+++ Objects/PingPongGenerator.cs
@@ -0,0 +1,26 @@
+
+
+namespace PingPong
+{
+
+  // A class that generates a list of numbers and then replaces certain numbers with strings
+  public class PingPongGenerator
+  {
+    // properties
+    // An int which will hold the parsed string passed in from user form
+
+    // An Array which will be generated based on the int which was parsed
+
+    // An Arraylist of strings and integers which will take values from the array, with certain numbers replaced by strings
+    // We can also do this using a dictionary of keys = Ints, values = Strings
+
+    // constructors, getters and setters
+
+
+    // other methods
+    // A method to generate an array of numbers based on an int that has been passed in
+
+
+    // A method to create a list of values based on the array, with certain numbers replaced by strings
+  }
+}
